b4507fed9dff3c287897aff7dc32724d7e5503d9
rafaelcsva <rafaelgyncastro63@gmail.com>
2021-03-30 19:55:32 -0300

    Dont use panic on error

--- gift4u-backend/db/db.go
+++ gift4u-backend/db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/SAMCRODE/gift4u-backend/config"
 
 	"github.com/go-pg/pg/v10"
@@ -13,8 +15,8 @@ func Init() {
 
 	opt, err := pg.ParseURL(conf.DATABASEURI)
 	if err != nil {
-		// fmt.Println("Database connection error")
-		panic(err)
+		fmt.Println("Database connection error")
+		// panic(err)
 	} else {
 		db = pg.Connect(opt)
 	}
