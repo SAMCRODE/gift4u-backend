30092f9161c0cbfd368a6a413afa32375b53e8eb
rafaelcsva <rafaelgyncastro63@gmail.com>
2021-04-18 18:57:40 -0300

    Add gingonic cors middleware; Update vendor

--- gift4u-backend/go.mod
+++ gift4u-backend/go.mod
@@ -6,7 +6,8 @@ go 1.16
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
-	github.com/gin-gonic/gin v1.4.0
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.5.0
 	github.com/go-pg/pg/v10 v10.9.0
 	github.com/joho/godotenv v1.3.0
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
--- gift4u-backend/server/router.go
+++ gift4u-backend/server/router.go
@@ -2,7 +2,7 @@ package server
 
 import (
 	"github.com/SAMCRODE/gift4u-backend/controllers"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +10,14 @@ func NewRouter() *gin.Engine {
 	router := gin.New()
 
 	router.Use(gin.Logger())
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
